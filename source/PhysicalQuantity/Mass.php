0c45898fd0bff764492c7901265c87c6f33b3683
Matthew Lawrence <matthew.lawrence@gen10.net>
2024-07-18 10:40:15 +0100

    Added new units of measure to the Mass class and updated MassTest to include tests for new measurements

--- source/PhysicalQuantity/Mass.php
+++ source/PhysicalQuantity/Mass.php
@@ -21,7 +21,7 @@ class Mass extends AbstractPhysicalQuantity
 
         static::addMissingSIPrefixedUnits(
             $kilogram,
-            1e-3,
+            0.001,
             '%pg',
             [
                 '%Pgram',
@@ -30,30 +30,125 @@ class Mass extends AbstractPhysicalQuantity
         );
 
         // Tonne (metric)
-        $newUnit = UnitOfMeasure::linearUnitFactory('t', 1e3);
+        $newUnit = UnitOfMeasure::linearUnitFactory('t', 1000);
         $newUnit->addAlias('ton');
         $newUnit->addAlias('tons');
         $newUnit->addAlias('tonne');
         $newUnit->addAlias('tonnes');
+        $newUnit->addAlias('mt');
+        $newUnit->addAlias('MT');
         static::addUnit($newUnit);
 
         // Pound
-        $newUnit = UnitOfMeasure::linearUnitFactory('lb', 4.5359237e-1);
+        $newUnit = UnitOfMeasure::linearUnitFactory('lb', 0.45359237);
         $newUnit->addAlias('lbs');
+        $newUnit->addAlias('Lbs');
         $newUnit->addAlias('pound');
         $newUnit->addAlias('pounds');
         static::addUnit($newUnit);
 
         // Ounce
-        $newUnit = UnitOfMeasure::linearUnitFactory('oz', 4.5359237e-1 / 16);
+        $newUnit = UnitOfMeasure::linearUnitFactory('oz', 0.45359237 / 16);
         $newUnit->addAlias('ounce');
         $newUnit->addAlias('ounces');
         static::addUnit($newUnit);
 
         // Stone
-        $newUnit = UnitOfMeasure::linearUnitFactory('st', 4.5359237e-1 * 14);
+        $newUnit = UnitOfMeasure::linearUnitFactory('st', 6.35029318);
         $newUnit->addAlias('stone');
         $newUnit->addAlias('stones');
+        static::addUnit($newUnit);
+
+        // Bales (500 lbs)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bales (500 lbs)', 226.7962);
+        $newUnit->addAlias('Bales (500)');
+        static::addUnit($newUnit);
+
+        // Bales (330 lbs)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bales (330 lbs)', 149.6855);
+        $newUnit->addAlias('Bales (330)');
+        static::addUnit($newUnit);
+
+        // Bags (60 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (60 kg)', 60);
+        $newUnit->addAlias('Bags (60)');
+        $newUnit->addAlias('Bags (60 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (46 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (46 kg)', 46);
+        $newUnit->addAlias('Bags (46)');
+        $newUnit->addAlias('Bags (46 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (69 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (69 kg)', 69);
+        $newUnit->addAlias('Bags (69)');
+        $newUnit->addAlias('Bags (69 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (30 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (30 kg)', 30);
+        $newUnit->addAlias('Bags (30)');
+        $newUnit->addAlias('Bags (30 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (55 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (55 kg)', 55);
+        $newUnit->addAlias('Bags (55)');
+        $newUnit->addAlias('Bags (55 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (56 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (56 kg)', 56);
+        $newUnit->addAlias('Bags (56)');
+        $newUnit->addAlias('Bags (56 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (57 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (57 kg)', 57);
+        $newUnit->addAlias('Bags (57)');
+        $newUnit->addAlias('Bags (57 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (65 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (65 kg)', 65);
+        $newUnit->addAlias('Bags (65)');
+        $newUnit->addAlias('Bags (65 kgs)');
+        static::addUnit($newUnit);
+
+        // Bags (100 kg)
+        $newUnit = UnitOfMeasure::linearUnitFactory('Bags (100 kg)', 100);
+        $newUnit->addAlias('Bags (100)');
+        $newUnit->addAlias('Bags (100 kgs)');
+        static::addUnit($newUnit);
+
+        // bu(s)
+        $newUnit = UnitOfMeasure::linearUnitFactory('bu(s)', 27.2155);
+        $newUnit->addAlias('Bushel (Soya)');
+        static::addUnit($newUnit);
+
+        // bu(c)
+        $newUnit = UnitOfMeasure::linearUnitFactory('bu(c)', 25.4);
+        $newUnit->addAlias('Bushel (Corn)');
+        static::addUnit($newUnit);
+
+        // bu(o)
+        $newUnit = UnitOfMeasure::linearUnitFactory('bu(o)', 14.515);
+        $newUnit->addAlias('Bushel (Oats)');
+        static::addUnit($newUnit);
+
+        // bu(w)
+        $newUnit = UnitOfMeasure::linearUnitFactory('bu(w)', 27.2155);
+        $newUnit->addAlias('Bushel (Wheat)');
+        static::addUnit($newUnit);
+
+        // Rolls
+        $newUnit = UnitOfMeasure::linearUnitFactory('Rolls', 500);
+        static::addUnit($newUnit);
+
+        // CWT
+        $newUnit = UnitOfMeasure::linearUnitFactory('CWT', 45.3592);
         static::addUnit($newUnit);
     }
 }
--- tests/PhysicalQuantity/MassTest.php
+++ tests/PhysicalQuantity/MassTest.php
@@ -115,4 +115,89 @@ class MassTest extends AbstractPhysicalQuantityTestCase
         $quantity = new Mass(14, 'pound');
         $this->assertEquals(1, $quantity->toUnit('st'));
     }
+
+    public function testBales500LbsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bales (500 lbs)');
+        $this->assertEquals( 0.2267962, $quantity->toUnit('t'));
+    }
+
+    public function testBales330LbsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bales (330 lbs)');
+        $this->assertEquals( 0.1496855, $quantity->toUnit('t'));
+    }
+
+    public function testBags60KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (60 kgs)');
+        $this->assertEquals( 0.06, $quantity->toUnit('t'));
+    }
+
+    public function testBags46KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (46 kgs)');
+        $this->assertEquals( 0.046, $quantity->toUnit('t'));
+    }
+
+    public function testBags69KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (69 kgs)');
+        $this->assertEquals( 0.069, $quantity->toUnit('t'));
+    }
+
+    public function testBags30KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (30 kgs)');
+        $this->assertEquals( 0.03, $quantity->toUnit('t'));
+    }
+
+    public function testBags55KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (55 kgs)');
+        $this->assertEquals( 0.055, $quantity->toUnit('t'));
+    }
+    public function testBags56KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (56 kgs)');
+        $this->assertEquals( 0.056, $quantity->toUnit('t'));
+    }
+
+    public function testBags57KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (57 kgs)');
+        $this->assertEquals( 0.057, $quantity->toUnit('t'));
+    }
+
+    public function testBags65KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (65 kgs)');
+        $this->assertEquals( 0.065, $quantity->toUnit('t'));
+    }
+
+    public function testBags100KgsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bags (100 kgs)');
+        $this->assertEquals( 0.1, $quantity->toUnit('t'));
+    }
+
+    public function testBushelSoyaToMetricTonnes(){
+        $quantity = new Mass(1, 'Bushel (Soya)');
+        $this->assertEquals( 0.0272155, $quantity->toUnit('t'));
+    }
+
+    public function testBushelCornToMetricTonnes(){
+        $quantity = new Mass(1, 'Bushel (Corn)');
+        $this->assertEquals( 0.0254, $quantity->toUnit('t'));
+    }
+
+    public function testBushelOatsToMetricTonnes(){
+        $quantity = new Mass(1, 'Bushel (Oats)');
+        $this->assertEquals( 0.014515, $quantity->toUnit('t'));
+    }
+
+    public function testBushelWheatToMetricTonnes(){
+        $quantity = new Mass(1, 'Bushel (Wheat)');
+        $this->assertEquals( 0.0272155, $quantity->toUnit('t'));
+    }
+
+    public function testCWTToMetricTonnes(){
+        $quantity = new Mass(1, 'CWT');
+        $this->assertEquals( 0.0453592, $quantity->toUnit('t'));
+    }
+
+
+    public function testRollToMetricTonnes(){
+        $quantity = new Mass(1, 'Roll');
+        $this->assertEquals( 0.0453592, $quantity->toUnit('t'));
+    }
 }
